179f1a85bcd09c505c859446b1ec0d815d1bb06f
KoalaTeen <marcovandervlag@outlook.com>
2023-02-25 19:40:43 +0100

    Made the program readable

--- overhoormachine.py
+++ overhoormachine.py
@@ -1,90 +1,90 @@
 import random
+import urllib.request
 
-#This function will read the .txt file with the words in it. The file is build up like this:
-# english word, spanish word,category
-#For example:
-# cheese,queso,food
-#After reading it will sort out the category and will return the words that are in that specific category.
+# This function reads a list of words from a URL and filters them by category
 def load_words(category):
-    with open('.\spanish_words.txt', 'r', encoding='utf-8') as f:
-        words = [line.strip().split(',') for line in f.readlines()]
+    # Open the URL and read its contents as a string
+    with urllib.request.urlopen('https://raw.githubusercontent.com/Marcovandervlag/Spaans_leren/main/spanish_words.txt') as f:
+        content = f.read().decode('utf-8')
+    
+    # Split the string into lines and parse each line as a list of values
+    words = [line.strip().split(',') for line in content.splitlines()]
+
+    # If a category was specified, filter the words to only include those in that category
     if category:
         words = [w for w in words if w[2] == category]
+    
+    # Return the filtered words
     return words
 
-#This function is the menu option you will get after starting the program.
-#It will let you choose between the categories given in the list below.
+# This function prompts the user to select a category and returns their choice
 def get_category():
+    # Define a dictionary mapping menu options to category names
+    options = {
+        '1': 'Korfball',
+        '2': 'Food',
+        '3': 'Drink',
+        '4': 'Hobbies',
+        '5': 'Greetings',
+        '6': 'Looks',
+        '7': 'Colors',
+        '8': 'Weather',
+        '9': 'Objects',
+        '10': 'Days',
+        '11': 'Months',
+        '12': 'vervoeging',
+    }
+
+    # Loop until the user selects a valid option
     while True:
         print("Select a category:")
-        print("1. Korfball")
-        print("2. Food")
-        print("3. Drink")
-        print("4. Hobbies")
-        print("5. greetings")
-        print("6. Looks")
-        print("7. Colors")
-        print("8. Weather")
-        print("9. objects")
-        print("10. Days")
-        print("11. Months")
-        print("12. Vervoeging")
+        # Print the menu options
+        for key, value in options.items():
+            print(f"{key}. {value}")
+        # Read the user's choice from the keyboard
         choice = input()
-        if choice == '1':
-            return 'Korfball'
-        elif choice == '2':
-            return 'Food'
-        elif choice == '3':
-            return 'Drink'
-        elif choice == '4':
-            return 'Hobbies'
-        elif choice == '5':
-            return 'Greetings'
-        elif choice == '6':
-            return 'Looks'
-        elif choice == '7':
-            return 'Colors'
-        elif choice == '8':
-            return 'Weather'
-        elif choice == '9':
-            return 'Objects'
-        elif choice == '10':
-            return 'Days'
-        elif choice == '11':
-            return 'Months'
-        elif choice == '12':
-            return 'vervoeging'
-        else:
-            print("Invalid choice, please try again.")
-#Makes all the words random. So there is (Almost) never the same quiz.
+        # If the choice is valid, return the corresponding category name
+        if choice in options:
+            return options[choice]
+        # Otherwise, print an error message and loop again
+        print("Invalid choice, please try again.")
+
+# This function selects a random word from a list of words
 def get_word(words):
     return random.choice(words)
 
-#This function will get the word of the translation and ask you what is the translation off it
+# This function prompts the user to translate a word and returns True if the translation is correct, False otherwise
 def translate(word):
     english_word, spanish_word, category = word
     guess = input(f"What is the Spanish word for {english_word}? ")
     return guess.lower() == spanish_word.lower()
 
-#This function will start de quiz/game and will gather all the correct answers and 
-#all the answers that you filled in incorrect.
+# This function plays the game, prompting the user to translate 10 random words from the specified category
 def play_game(category=None):
+    # Load the words for the specified category (or all categories if none is specified)
     words = load_words(category)
+    # Initialize counters for the number of correct and total answers
     correct = 0
     total = 0
-    while True:
+    # Loop until 10 words have been translated
+    while total < 10:
+        # Select a random word from the list
         word = get_word(words)
+        # Prompt the user to translate the word
         if translate(word):
+            # If the translation is correct, increment the correct counter and print a success message
             print("Correct!")
             correct += 1
         else:
+            # Otherwise, print the correct translation
             print(f"Incorrect. The correct answer is {word[1]}")
+        # Increment the total counter
         total += 1
-        if total == 10:
-            break
+    # Print the final score
     print(f"Game over! You scored {correct} out of {total}.")
 
-#This will start the whole program.
+# This is the main entry point for the program
 if __name__ == '__main__':
     category = get_category()
     play_game(category)
+    # Prompt the user to
