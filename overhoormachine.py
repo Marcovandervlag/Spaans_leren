0d02606e2cb45476275bd1d4b8dbcc4b88bfe272
Marco van der Vlag <107155973+Marcovandervlag@users.noreply.github.com>
2023-02-26 12:03:55 +0100

    Json compatible
    Made it so there is a compatibility with the json file. SO it is more readable for a human eye

--- overhoormachine.py
+++ overhoormachine.py
@@ -1,22 +1,20 @@
 import random
 import urllib.request
-
+import json
 # This function reads a list of words from a URL and filters them by category
 def load_words(category):
     # Open the URL and read its contents as a string
-    with urllib.request.urlopen('https://raw.githubusercontent.com/Marcovandervlag/Spaans_leren/main/spanish_words.txt') as f:
-        content = f.read().decode('utf-8')
-    
-    # Split the string into lines and parse each line as a list of values
-    words = [line.strip().split(',') for line in content.splitlines()]
-
+    with urllib.request.urlopen('https://raw.githubusercontent.com/Marcovandervlag/Spaans_leren/main/spanish_words.json') as f:
+        content = f.read().decode('utf-8')    
+    # Parse the string as a JSON object
+    words = json.loads(content)
     # If a category was specified, filter the words to only include those in that category
     if category:
-        words = [w for w in words if w[2] == category]
-    
+        words = [(w['english'], w['spanish'], w['category']) for w in words if w['category'] == category]
+    else:
+        words = [(w['english'], w['spanish'], w['category']) for w in words]    
     # Return the filtered words
     return words
-
 # This function prompts the user to select a category and returns their choice
 def get_category():
     # Define a dictionary mapping menu options to category names
@@ -34,7 +32,6 @@ def get_category():
         '11': 'Months',
         '12': 'vervoeging',
     }
-
     # Loop until the user selects a valid option
     while True:
         print("Select a category:")
@@ -48,17 +45,14 @@ def get_category():
             return options[choice]
         # Otherwise, print an error message and loop again
         print("Invalid choice, please try again.")
-
 # This function selects a random word from a list of words
 def get_word(words):
     return random.choice(words)
-
 # This function prompts the user to translate a word and returns True if the translation is correct, False otherwise
 def translate(word):
     english_word, spanish_word, category = word
     guess = input(f"What is the Spanish word for {english_word}? ")
     return guess.lower() == spanish_word.lower()
-
 # This function plays the game, prompting the user to translate 10 random words from the specified category
 def play_game(category=None):
     # Load the words for the specified category (or all categories if none is specified)
@@ -82,9 +76,8 @@ def play_game(category=None):
         total += 1
     # Print the final score
     print(f"Game over! You scored {correct} out of {total}.")
-
 # This is the main entry point for the program
 if __name__ == '__main__':
     category = get_category()
     play_game(category)
-    # Prompt the user to
+    # Prompt the user to play
